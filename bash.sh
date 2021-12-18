25cf787ca91da8d9094ccb0721958ac35b736e0f
Kaio Santos <60193295+KaioSant@users.noreply.github.com>
2021-12-18 08:33:20 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-menu=output "\e[36m'
+menu=echo -e "\e[36m'
 0)Inicializar a instalação
 1)Recusar Instalação
 2)Informações do script'\e[0m"
 
 echo "$menu"
 
-ini="\e[36m'Inicializando instalação.....'\e[0m"
+ini=echo -e "\e[36m'Inicializando instalação.....'\e[0m"
 recu="Script Cancelado com sucesso!"
 
-info="\e[36m'
+info=echo -e "\e[36m'
 ==============================================
 Script Criado por kaio santos
 Script versão: 0.1
@@ -22,25 +22,25 @@ read -p "Escolha uma Opção: " OPCAO
 case "$OPCAO" in
       0)
       echo ""
-output "> vamos instalar o apache..."
+echo "> vamos instalar o apache..."
 sudo apt update
 sudo apt install apache2
 sudo ufw app list
 sudo ufw app info "Apache Full"
 sudo ufw allow "Apache Full"
-output "> Servidor Apache2 instalado com sucesso! entre com seu subdominio para confirmar."
-output "instalando o curl & o icanhazip"
+echo "> Servidor Apache2 instalado com sucesso! entre com seu subdominio para confirmar."
+echo "instalando o curl & e verificando com icanhazip"
 sudo apt install curl
 curl http://icanhazip.com
-output "> instalado com sucesso. Agora vamos instalar o MySQL!"
+echo "> instalado com sucesso. Agora vamos instalar o MySQL!"
 sudo apt install mysql-server
 sudo mysql_secure_installation
 sudo exit
-output "> Vamos instalar o seu PHP, por favor aguarde."
+echo "> Vamos instalar o seu PHP, por favor aguarde."
 sudo apt install php libapache2-mod-php php-mysql
 
 
-output "> A parte web foi instalada com sucesso, vamos instalar seu painel agora."
+echo "> A parte web foi instalada com sucesso, vamos instalar seu painel agora."
 
 cd /var/www/html/
 wget http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
@@ -50,14 +50,14 @@ cd multicraft
       ;;
       
       1)
-      output "$recu"
+      echo "$recu"
       exit;
       ;;
       2)
-      output "$info"
+      echo "$info"
       ;;
       *)
-      output "Opção $OPCAO desconhecida!"
+      echo "Opção $OPCAO desconhecida!"
       exit 1
       ;;
 esac
