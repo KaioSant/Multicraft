e617ee06a5793d5282263a6e5b81ea6dbcb6958e
Kaio Santos <60193295+KaioSant@users.noreply.github.com>
2021-12-18 19:33:55 -0300

    Update painel.sh

--- painel.sh
+++ painel.sh
@@ -4,3 +4,7 @@ sudo wget http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
 tar xvzf multicraft.tar.gz
 cd multicraft
 ./setup.sh
+
+clear
+
+echo "\e[36m'Painel Instalado Com sucesso!'\e[0m"
