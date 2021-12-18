8e75a33a0f89cbcf2d8c7ef48f7493997b032151
Kaio Santos <60193295+KaioSant@users.noreply.github.com>
2021-12-18 08:24:52 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,7 +21,7 @@ Script versão: 0.1
 Github do kaio: https://github.com/KaioSant
 =============================================="
 
-output "Escolha uma Opção: " OPCAO
+read -p "Escolha uma Opção: " OPCAO
 
 case "$OPCAO" in
       0)
