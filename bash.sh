6a49962d4a7d87ca1496e11f192a97a95d80ebaf
Kaio Santos <60193295+KaioSant@users.noreply.github.com>
2021-12-18 08:21:48 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-clear
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
 
 menu="
 0)Inicializar a instalação
@@ -19,30 +21,30 @@ Script versão: 0.1
 Github do kaio: https://github.com/KaioSant
 =============================================="
 
-read -p "Escolha uma Opção" OPCAO
+output -p "Escolha uma Opção: " OPCAO
 
 case "$OPCAO" in
       0)
       echo ""
-echo "> vamos instalar o apache..."
+output "> vamos instalar o apache..."
 sudo apt update
 sudo apt install apache2
 sudo ufw app list
 sudo ufw app info "Apache Full"
 sudo ufw allow "Apache Full"
-echo "> Servidor Apache2 instalado com sucesso! entre com seu subdominio para confirmar."
-echo "instalando o curl & o icanhazip"
+output "> Servidor Apache2 instalado com sucesso! entre com seu subdominio para confirmar."
+output "instalando o curl & o icanhazip"
 sudo apt install curl
 curl http://icanhazip.com
-echo "> instalado com sucesso. Agora vamos instalar o MySQL!"
+output "> instalado com sucesso. Agora vamos instalar o MySQL!"
 sudo apt install mysql-server
 sudo mysql_secure_installation
 sudo exit
-echo "> Vamos instalar o seu PHP, por favor aguarde."
+output "> Vamos instalar o seu PHP, por favor aguarde."
 sudo apt install php libapache2-mod-php php-mysql
 
 
-echo "> A parte web foi instalada com sucesso, vamos instalar seu painel agora."
+output "> A parte web foi instalada com sucesso, vamos instalar seu painel agora."
 
 cd /var/www/html/
 wget http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
@@ -52,14 +54,14 @@ cd multicraft
       ;;
       
       1)
-      echo "$recu"
+      output "$recu"
       exit;
       ;;
       2)
-      echo "$info"
+      output "$info"
       ;;
       *)
-      echo "Opção $OPCAO desconhecida!"
+      output "Opção $OPCAO desconhecida!"
       exit 1
       ;;
 esac
